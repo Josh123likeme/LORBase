656b7aa598eb981399abbaf71f3181c241d77451
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2022-10-14 11:56:17 +0100

    Add files via upload

---
+++ src/me/Josh123likeme/LORBase/FrameData.java
@@ -0,0 +1,9 @@
+package me.Josh123likeme.LORBase;
+
+public class FrameData {
+
+	public int Height;
+	public int Width;
+	public double DeltaFrame;
+	
+}
---
+++ src/me/Josh123likeme/LORBase/GameHolder/Lobby.java
@@ -0,0 +1,80 @@
+package me.Josh123likeme.LORBase.GameHolder;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.Josh123likeme.LORBase.Game;
+import me.Josh123likeme.LORBase.Main;
+import me.Josh123likeme.LORBase.UI.Menu;
+
+class Lobby {
+
+	private Game game;
+	
+	private Menu mainMenu;
+	private Save save;
+	
+	private List<Menu> menuStack = new ArrayList<Menu>();
+	
+	public Lobby(Save save) {
+		
+		this.save = save;
+		
+		game = Main.game;
+		
+		initMenus();
+		
+		menuStack.add(mainMenu);
+		
+	}
+	
+	private void initMenus() {
+		
+		mainMenu = new Menu();
+		mainMenu.createButton("ENTER", "Enter Labyrinth", 0.7, 0.8, 0.25, 0.15);
+		
+	}
+	
+	public void render(Graphics g) {
+
+		peekMenuStack().render(g);
+		
+	}
+	
+	public void detectMenuClicks() {
+		
+		Menu topMenu = peekMenuStack();
+		
+		String button = game.mouseWitness.isLeftClicked() ? topMenu.getButtonAt(game.mouseWitness.getMouseX(), game.mouseWitness.getMouseY()) : null;
+		
+		if (button == null) return;
+		
+		if (button == "BACK") {
+			
+			popMenuStack();
+			
+		}
+		
+		if (topMenu == mainMenu && button == "ENTER") {
+			
+			save.startWorld();
+			save.closeLobby();
+			
+		}
+		
+	}
+	
+	private Menu peekMenuStack() {
+		
+		return menuStack.get(menuStack.size() - 1);
+		
+	}
+	
+	private void popMenuStack() {
+		
+		menuStack.remove(menuStack.size() - 1);
+		
+	}
+	
+}
