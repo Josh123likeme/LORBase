439cebbf453f69162eae87209e910dbd5d869e1f
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2022-11-01 23:06:46 +0000

    Delete src directory

--- src/me/Josh123likeme/LORBase/EntityHolder/IHasInventory.java
+++
@@ -1,9 +0,0 @@
-package me.Josh123likeme.LORBase.EntityHolder;
-
-import me.Josh123likeme.LORBase.ItemHolder.Inventory;
-
-public interface IHasInventory {
-
-	public Inventory getInventory();
-	
-}
