40dcf636df1863b0590309ae7cffdd2372588c42
Josh123likeme <79576357+Josh123likeme@users.noreply.github.com>
2022-12-07 18:33:40 +0000

    Delete src directory

--- src/me/Josh123likeme/LORBase/EntityHolder/ICollidable.java
+++
@@ -1,5 +0,0 @@
-package me.Josh123likeme.LORBase.EntityHolder;
-
-public interface ICollidable {
-	
-}
--- src/me/Josh123likeme/LORBase/EntityHolder/ISleep.java
+++
@@ -1,7 +0,0 @@
-package me.Josh123likeme.LORBase.EntityHolder;
-
-public interface ISleep {
-
-	public boolean isSleeping();
-	
-}
